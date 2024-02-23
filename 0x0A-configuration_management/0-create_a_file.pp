8754a58178b62e1e20a43eb623f58c3a64620aad
0xNuru <nurudeenmyself@yahoo.com>
2024-02-23 14:15:50 +0100

    add Hello, Puppet!

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# This puppet manifest is used to create a file in /tmp
+
+file { '/tmp/school':
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+# This dir contains all the tasks for 0x0A-configuration_management. We are practicing CM using a tool called Puppet
