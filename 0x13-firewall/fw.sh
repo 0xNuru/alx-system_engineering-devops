a17fe956b98cae802e911a17dd829aa8b22b1c14
0xNuru <nurudeenmyself@yahoo.com>
2024-03-18 21:32:02 +0100

    add readme and task 00

--- 0x13-firewall/fw.sh
+++ 0x13-firewall/fw.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 sudo apt install ufw -y
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw allow ssh
-sudo ufw enable
+sudo ufw enable -y
 sudo ufw allow http
 sudo ufw allow https
