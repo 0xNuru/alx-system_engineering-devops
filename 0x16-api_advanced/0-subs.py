283269e1d23a2bca454b6b0a7dd5d071a04cc4a7
0xNuru <nurudeenmyself@yahoo.com>
2024-04-09 17:43:55 +0100

    add script to query subscribers of a subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""a script that queries the Reddit API and returns the number of subscribers"""
+import requests
+
+
+
+def number_of_subscribers(subreddit):
+    """print number of subscribers to a subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "RedditSubscribersCountBot/0.1 by nurvdeen"}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code in (301, 302, 307, 404):
+        return 0
+    
+    try:
+        data = response.json()
+        if "data" in data and "subscribers" in data["data"]:
+            return data["data"]["subscribers"]
+        else:
+            return 0
+    except ValueError:
+        # In case the response isn't a valid JSON
+        return 0
+
