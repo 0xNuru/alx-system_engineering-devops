453fc035813fed1ed0344dcaf6d9f6c2a0349d4e
0xNuru <nurudeenmyself@yahoo.com>
2024-04-09 17:57:01 +0100

    revert to original code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,16 +2,22 @@
 """a script that queries the Reddit API and return the number of subs"""
 import requests
 
+
 def number_of_subscribers(subreddit):
     """print number of subscribers to a subreddit"""
-    url = "https://reddit.com/r/{}/about.json".format(subreddit)
-    userAgent = "Mozilla/5.0"
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "RedditSubscribersCountBot/0.1 by nurvdeen"}
 
-    response = requests.get(url, headers={"user-agent": userAgent})
-    if not response:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         return 0
-    retValue = response.json().get('data').get('subscribers')
-    if retValue:
-        return retValue
-    else:
+
+    try:
+        data = response.json()
+        if "data" in data and "subscribers" in data["data"]:
+            return data["data"]["subscribers"]
+        else:
+            return 0
+    except ValueError:
+        # In case the response isn't a valid JSON
         return 0
