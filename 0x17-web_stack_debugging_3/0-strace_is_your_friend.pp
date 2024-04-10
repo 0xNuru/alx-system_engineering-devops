bd69b944ca63a116f28e453ff70f242bef2e9a7b
0xNuru <nurudeenmyself@yahoo.com>
2024-04-10 17:53:13 +0100

    add puppet script

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# a puppet script as a result of using strace,
+# find out why Apache is returning a 500 error and fix
+exec {'replace':
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+}
